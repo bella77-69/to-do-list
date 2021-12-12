e0601503955e21dacede28cdfc07f4bdee8abae0
chantelle mckenzie <mrspasceri@hotmail.com>
2021-12-11 20:11:09 -0800

    added to-do list, to-do form

---
+++ src/components/ToDoForm.jsx
@@ -0,0 +1,38 @@
+import React, { useReducer } from "react";
+import "./ToDoForm.scss";
+
+export default function TodoForm({ task, createTodo }) {
+  const [userInput, setUserInput] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    {
+      task: ""
+    }
+  );
+
+  const handleChange = evt => {
+    setUserInput({ [evt.target.name]: evt.target.value });
+  };
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    const newTodo = { task: userInput.task, completed: false };
+    createTodo(newTodo);
+    setUserInput({ task: "" });
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <label className="form-task">New todo</label>
+      <input className='form-input'
+        value={userInput.task}
+        onChange={handleChange}
+        id="task"
+        type="text"
+        name="task"
+        placeholder="New Todo"
+      />
+      <button className="form-button">Add Todo</button>
+    </form>
+  );
+}
+
---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import Todo from "./ToDo";
+import TodoForm from "./ToDoForm";
+
+
+export default function ToDoList() {
+  const [todos, setTodos] = useState([
+    {task: "task 1", completed: false },
+    {task: "task 2", completed: true }
+  ]);
+
+  const create = newTodo => {
+    console.log(newTodo);
+    setTodos([...todos, newTodo]);
+  };
+
+  const remove = id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const update = (id, updtedTask) => {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, task: updtedTask };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const toggleComplete = id => {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const todosList = todos.map(todo => (
+    <Todo
+      toggleComplete={toggleComplete}
+      update={update}
+      remove={remove}
+      key={todo.id}
+      todo={todo}
+    />
+  ));
+
+  return (
+    <div className="list">
+      <h1 className="list-title">
+        Todo List <span className="list-subtitle">A simple React Todo List App</span>
+      </h1>
+      <ul className="list-list">{todosList}</ul>
+      <TodoForm createTodo={create} />
+    </div>
+  );
+}
+
+
